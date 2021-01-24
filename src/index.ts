3b0fd57c71044405b9ff108e05991015c7b13d5e
Nikhil Bhintade <nikhil.bhintade0104@gmail.com>
2021-01-24 20:42:05 +0530

    graphql server with express & typescript

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,32 @@
-import { MikroORM } from "@mikro-orm/core";
-import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+import express from "express";
 import mikroConfig from "./mikro-orm.config";
+
+import { __prod__ } from "./constants";
+import { ApolloServer } from "apollo-server-express";
+import { MikroORM } from "@mikro-orm/core";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
     
     const orm = await MikroORM.init(mikroConfig);
 
     await orm.getMigrator().up();
+    
+    const app = express();
 
-    const post = orm.em.create(Post, {title: 'My first Post'});
-    await orm.em.persistAndFlush(post);
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        }),
+    })
+
+    apolloServer.applyMiddleware({ app }); 
+
+    app.listen(4000, () => {
+        console.log('listening on port localhost:4000');
+    })
 };
 
 main().catch((err) => {
